f1ea48d59e30ff57831a19ad1b9047cf32c23467
Guilherme Bastos <126486967+guilhermebastos2002@users.noreply.github.com>
2024-10-01 20:19:16 -0300

    final

---
+++ Ap1_GuilhermeBastos/src/main/java/com/Ap1_GuilhermeBastosApplication.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ap1_GuilhermeBastosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Ap1_GuilhermeBastosApplication.class, args);
+    }
+}
--- Ap1_GuilhermeBastos/src/main/java/com/trabalhoap1/Ap1_GuilhermeBastos/Ap1GuilhermeBastosApplication.java
+++ Ap1_GuilhermeBastos/src/main/java/com/trabalhoap1/Ap1_GuilhermeBastos/Ap1GuilhermeBastosApplication.java
@@ -1,13 +1,30 @@
-package com.trabalhoap1.Ap1_GuilhermeBastos;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//public class ClienteServiceTest {
 
-@SpringBootApplication
-public class Ap1GuilhermeBastosApplication {
+   // @Autowired
+    //private ClienteService clienteService; // Supondo que você tenha um serviço chamado ClienteService
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ap1GuilhermeBastosApplication.class, args);
-	}
+    // @Test
+    // public void should_not_create_client_if_cpf_has_more_than_nine_characters() {
+    
+    //    Cliente clienteInvalido = new Cliente();
+    //    clienteInvalido.setNome("Cliente Teste");
+    //    clienteInvalido.setEmail("cliente@teste.com");
+    //    clienteInvalido.setCpf("12345678901"); // CPF com 11 caracteres, o que é inválido
+    //   clienteInvalido.setDataNascimento(LocalDateTime.now()); // Data de nascimento válida
+    //    clienteInvalido.setTelefone("123456789");
 
-}
+    //    Exception exception = Assertions.assertThrows(Exception.class, () -> {
+    //        clienteService.createCliente(clienteInvalido); // Espera uma exceção ao tentar criar o cliente
+    //   });
+
+        // Verifique a mensagem da exceção
+    //    String expectedMessage = "O CPF deve ter no máximo 9 caracteres.";
+    //    String actualMessage = exception.getMessage();
+
+    //    Assertions.assertTrue(actualMessage.contains(expectedMessage), "A mensagem de erro não está correta."); // Verifica se a mensagem está correta
+    //}
+//}
